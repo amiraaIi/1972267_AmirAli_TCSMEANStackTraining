b43d28aa1aad90f135ff529083cd86978e443886
Amir Ali <amir.a.ali071@gmail.com>
2021-03-16 16:08:09 -0500

    Completed most functionality of the site

--- simply-blogging/script.js
+++ simply-blogging/script.js
@@ -2,10 +2,13 @@ function addBlog() {
     storeBlog();
     var data = JSON.parse(sessionStorage.getItem("blogInfo"));
 
+    // Empty the div before it gets reprinted as the old logs won't show
+    // on reload otherwise
+    document.getElementById("blogDiv").innerHTML = "";
+
     data.forEach(element => {
         populateBlogs(element);
     });
-    
 }
 
 function readFormData() {
@@ -13,10 +16,9 @@ function readFormData() {
 
     obj.title = document.getElementById("title").value;
     obj.desc = document.getElementById("desc").value;
-    var image = document.getElementById("image").files[0];
     
-    if (image != undefined) {
-        obj.imageInfo = image.name;
+    if (document.getElementById("image").files[0] != undefined) {
+        obj.imageInfo = document.getElementById("image").files[0].name;
     }
 
     return obj;
@@ -24,6 +26,11 @@ function readFormData() {
 
 function storeBlog() {
     var data = readFormData();
+
+    if(data.title == "" || data.desc == "") {
+        alert("Please enter both a title and a description.");
+        return;
+    }
 
     resetData();
 
@@ -39,5 +46,19 @@ function storeBlog() {
 }
 
 function populateBlogs(data) {
-    blog.innerHTML += "<br>" + blogObj.title + "<br>" + blogObj.desc;
+    var blog = document.getElementById("blogDiv");
+
+    if(data.imageInfo != undefined) {
+        blog.innerHTML += "<br><div class='card'><img src='" + data.imageInfo + "'><div class='card-body'><h3>" + data.title + "</h3><br><hr><p class='card-text'>" + data.desc + "</p><br></div></div>";
+    }
+    else {
+        blog.innerHTML += "<br><div class='card'><div class='card-body'><h3>" + data.title + "</h3><br><hr><p class='card-text'>" + data.desc + "</p><br></div></div>";
+    }
+}
+
+// Clearing form fields
+function resetData() {
+    document.getElementById("title").value = "";
+    document.getElementById("desc").value = "";
+    document.getElementById("image").value = "";
 }
