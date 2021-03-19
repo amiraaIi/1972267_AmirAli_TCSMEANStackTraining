2bee1c4d8ebca9bc9163975cd2d2b2759b161c96
Amir Ali <amir.a.ali071@gmail.com>
2021-03-19 18:20:59 -0500

    Added functionality to buttons on the webpage as well as the cart page. Some final touches need to be made in order for the site to actually fully function.

--- cart-management/script.js
+++ cart-management/script.js
@@ -1,88 +1,63 @@
-class cartItem {
-    name: string;
-    price: number;
-
-    constructor(name: string, price: number) {
+var cartItem = /** @class */ (function () {
+    function cartItem(name, price) {
         this.name = name;
         this.price = price;
     }
-}
-
-function addToCart(name: string, price: number) {
-    let item = new cartItem(name, price);
-
+    return cartItem;
+}());
+function addToCart(name, price) {
+    var item = new cartItem(name, price);
     storeItem(item);
-
     setCartTotal();
 }
-
-function storeItem(item: cartItem) {
+function storeItem(item) {
     // Load old items into var
-    let oldCart = JSON.parse(sessionStorage.getItem("cartInfo"));
-
+    var oldCart = JSON.parse(sessionStorage.getItem("cartInfo"));
     // If there were no old items, make the var into an empty array
-    if(oldCart == null) {
+    if (oldCart == null) {
         oldCart = [];
     }
-
     oldCart.push(item);
-
     sessionStorage.setItem("cartInfo", JSON.stringify(oldCart));
 }
-
 function insertNewItem(item) {
     var table = document.getElementById("cartList");
     var body = table.getElementsByTagName("tbody")[0];
     var newRow = body.insertRow(body.rows.length);
     var cell1 = newRow.insertCell(0);
     var cell2 = newRow.insertCell(1);
-
     cell1.innerHTML = item.name;
     cell2.innerHTML = "$" + item.price;
 }
-
 // Repeatedly insert new rows into the table until all the items are added
 function populateCart() {
     var obj = sessionStorage.getItem("cartInfo");
     var data = JSON.parse(obj);
- 
-    data.forEach(element => {
-          insertNewItem(element);
+    data.forEach(function (element) {
+        insertNewItem(element);
     });
-
     insertNewItem(getTotal(data));
 }
-
 // Return total object
-function getTotal(obj: cartItem[]): cartItem {
-    let total: number = 0;
-
-    obj.forEach(element => {
+function getTotal(obj) {
+    var total = 0;
+    obj.forEach(function (element) {
         total += element.price;
-  });
-
-  let totalObj = new cartItem("<b>Total</b>", total);
-
-  return totalObj;
+    });
+    var totalObj = new cartItem("<b>Total</b>", total);
+    return totalObj;
 }
-
 function setCartTotal() {
-    let cart = JSON.parse(sessionStorage.getItem("cartInfo"));
-
-    let total = document.getElementById("total");
-
-    if(cart == null) {
+    var cart = JSON.parse(sessionStorage.getItem("cartInfo"));
+    var total = document.getElementById("total");
+    if (cart == null) {
         total.innerHTML = "Cart Total: Empty";
         return;
     }
-
     total.innerHTML = "Cart Total: " + getTotal(cart).price;
 }
-
 function emptyCart() {
-    let cart = null;
-
+    var cart = null;
     sessionStorage.setItem("cartInfo", JSON.stringify(cart));
-
     setCartTotal();
 }
